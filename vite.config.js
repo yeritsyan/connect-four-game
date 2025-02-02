cc61feecfb2ffd7b3937a38bd08f078b4db0158a
yeritsyan <28714090+yeritsyan@users.noreply.github.com>
2025-02-03 00:00:08 +0100

    added env files for better structure

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: process.env.NODE_ENV === "production" ? "/connect-four-game/" : "/",
+  base: process.env.VITE_BASE_PATH,
   server: {
     port: 5173, // Default port, change if needed
     open: true, // Automatically opens the browser on start
