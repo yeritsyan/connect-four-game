bc4f2aee08cf41fa8fcac32a27353b4219807a11
yeritsyan <28714090+yeritsyan@users.noreply.github.com>
2025-02-02 23:03:31 +0100

    modify base url  for gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/connect-four-game">
     <App />
   </BrowserRouter>
 );
