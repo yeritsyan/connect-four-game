516793f2878b82d7f305a5655541cceff2d96332
yeritsyan <28714090+yeritsyan@users.noreply.github.com>
2025-02-03 20:07:24 +0100

    add eslint to project and pipeline

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+  },
+  {
+    ignores: ["dist/**"], // Ignore all files inside dist
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    // Disable the 'react/react-in-jsx-scope' rule for React 17+
+    rules: {
+      "react/react-in-jsx-scope": "off", // Disable rule for React in scope
+      "react/prop-types": "off", // Optionally disable prop-types check if using TypeScript
+    },
+  },
+];
